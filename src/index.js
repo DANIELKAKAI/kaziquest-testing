743c766b9bc280b30ea4176a82c6a939bd300536
daniel kakai <danielkakai3@gmail.com>
2019-03-13 13:57:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter,Switch} from 'react-router-dom';
+import {} from 'react-router-dom';
 import {render} from 'react-snapshot';
 
-render(
-<BrowserRouter>
-<Switch>
-<App />
-</Switch>
-</BrowserRouter>
-
-, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
